5bb6f211b2fb3dac4523002613602519e7a26264
Fong <fongc2@pllwskifah1.mskcc.org>
2021-02-08 20:52:48 -0500

    Updating path variables and demo documentation

--- constants_darwin_pathology.py
+++ constants_darwin_pathology.py
@@ -4,12 +4,12 @@ constants_darwin_pathology.py
 Defined darwin data filename constants to use across the project
 """
 # PATH LOCATION OF DATA FILES
-pathname = '/Users/fongc2/Documents/github/MSK/DARWIN_ETL/data'
+pathname = '/mind_data/cdm_data/pathology'
 
 # Pathology Segmentation tasks
 ## Raw files
 ### Pathology data from DDP
-fname_path_ddp = 'Darwin_Discovery_Pathology_Reports_20201014.csv'
+fname_path_ddp = 'Darwin_Discovery_Pathology_Reports_20201207_f.tsv'
 
 ### Pathology table from copath
 fname_path_synoptic = 'synoptic_data_impact_patients.csv'
@@ -31,3 +31,14 @@ fname_darwin_path_dmp = 'table_pathology_impact_dmp_extraction.csv'
 fname_darwin_path_clean_parsed = 'table_pathology_surgical_samples_parsed.csv'            # Filename of processed pathology table (variable: fname_darwin_path)
 fname_darwin_path_clean_parsed_specimen = 'table_pathology_surgical_samples_parsed_specimen.csv'            # Filename of parsed specimen pathology
 fname_darwin_path_clean_parsed_specimen_impact_only = 'table_pathology_impact_only_parsed_specimen.csv'
+
+
+## Annotations
+### Table of MSK-IMPACT to source (Surgical/Cytology) accessions
+fname_accessions = 'path_accessions.csv'
+### Table of MSK-IMPACT to date of procedure
+fname_spec_part_dop = 'pathology_spec_part_dop.csv'
+### Table combining MSK-IMPACT report accession, source accession, date of procedure
+fname_combine_dop_accession = 'pathology_dop_impact_summary.csv'
+### File "fname_combine_dop_accession" with annotation for source of extraction (IMPACT report, surgical, IR)
+fname_dop_anno = 'table_pathology_impact_sample_summary_dop_anno.csv'
