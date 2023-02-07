7e374408e670e56005e973ce5ea1b2906bef405d
Chris Fong <fongc2@mskcc.org>
2023-02-07 11:34:59 -0500

    bug fix on file name use

--- annotations/pathology_extract_dop_impact_wrapper.py
+++ annotations/pathology_extract_dop_impact_wrapper.py
@@ -306,7 +306,7 @@ def main():
 
     # Extract source accession number
     obj_p = CombineAccessionDOPImpact(fname_minio_env=c_dar.minio_env,
-                                      fname_accession=c_dar.fname_accessions,
+                                      fname_accession=c_dar.fname_path_accessions,
                                       fname_dop=c_dar.fname_spec_part_dop,
                                       fname_path=c_dar.fname_path_clean,
                                       fname_save=c_dar.fname_combine_dop_accession)
