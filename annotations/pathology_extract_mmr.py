121be36ee59fe1a697fc2ef9709877dd92235ce3
Chris Fong <fongc2@mskcc.org>
2024-03-05 12:43:14 -0500

    Adding files from server backup

---
+++ annotations/pathology_extract_mmr.py
@@ -0,0 +1,70 @@
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+from pandas import Series
+import re
+
+sys.path.insert(0,  os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..', 'cdm-utilities')))
+sys.path.insert(0,  os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..', 'cdm-utilities', 'minio_api')))
+from minio_api import MinioAPI
+from utils import read_minio_api_config
+from data_classes_cdm import CDMProcessingVariables as c_dar
+
+
+## Constants
+FNAME_SAVE = c_dar.fname_path_mmr
+FNAME_PATH = c_dar.fname_pathology
+FNAME_MINIO_ENV = c_dar.minio_env
+COLS_SAVE = ['MRN','Accession Number','Path Procedure Date','MMR_ABSENT']
+
+obj_minio = MinioAPI(fname_minio_env=FNAME_MINIO_ENV)
+obj = obj_minio.load_obj(path_object=FNAME_PATH)
+df_path = pd.read_csv(obj, sep='\t', low_memory=False)
+'''
+     MLH1:     staining absent in tumor
+     PMS2:     staining absent in tumor
+     MSH2:     staining present in tumor
+     MSH6:     staining present in tumor'''
+
+
+def extractMMR(s):
+    factors = ['MLH1','PMS2','MSH2','MSH6']
+    for f in factors:
+        if f in s:
+            tokens = s.split(f)
+            for i, token in enumerate(tokens[1:]):
+                statement = '--'+token[:50] +'***'+ tokens[i][-50:]+'--'
+                absentLoc = max(statement.lower().find('absent'),
+                                statement.lower().find('loss'),
+                               statement.lower().find('lost'))
+                presentLoc= statement.lower().find('present')
+                if absentLoc>=0 and presentLoc>=0:
+                    if absentLoc<presentLoc:
+                        return True
+                if absentLoc>=0 and presentLoc<0:
+                        return True
+    return False
+
+
+filter_mmr = df_path['path_prpt_p1'].fillna('').str.contains('MLH1|PMS2|MSH2|MSH6',regex=True,case=False)
+filter_mnumber = ~df_path['Accession Number'].str.contains('M') 
+df_mmr = df_path[filter_mmr & filter_mnumber].copy()
+df_mmr['MMR_ABSENT'] = df_mmr['path_prpt_p1'].apply(extractMMR)
+df_save = df_mmr[COLS_SAVE]
+
+# Save to Minio
+obj_minio.save_obj(
+    df=df_save, 
+    path_object=FNAME_SAVE, 
+    sep='\t'
+)
+
+# df_mmr[['MRN','Accession Number','Path Procedure Date','MMR_ABSENT']].to_csv('MMR.csv',index=False)
+
+# bytedata=df_mmr[COLS_SAVE].to_csv(index=False).encode('utf-8')
+# bufferdata=BytesIO(bytedata)
+# client.put_object('cdm-data', 'pathology/pathology_mmr_calls.csv',
+#                   data=bufferdata,
+#                   length=len(bytedata),content_type='application/csv')
---
+++ pipeline/cbio_pdl1_summary.py
@@ -0,0 +1,128 @@
+#Import the requisite library
+import sys
+sys.path.insert(0, '/mind_data/cdm_repos/cdm-utilities/')
+sys.path.insert(0, '/mind_data/cdm_repos/cdm-utilities/minio_api')
+import re
+from data_classes_cdm import CDMProcessingVariables as var
+import pandas as pd
+from minio_api import MinioAPI
+from utils import mrn_zero_pad, convert_to_int
+
+
+FNAME_PDL1 = var.fname_path_pdl1
+FNAME_MAP = var.fname_dop_anno
+FNAME_MINIO_ENV = var.minio_env
+FNAME_SAVE_PATIENT = var.fname_path_pdl1_summary_patient
+FNAME_SAVE_SAMPLE = var.fname_path_pdl1_summary_sample
+
+
+def _load_data(
+    obj_minio,
+    fname_pdl1,
+    fname_map
+):
+    print('Loading %s' % fname_pdl1)
+    obj = obj_minio.load_obj(path_object=fname_pdl1)
+    df_pdl1 = pd.read_csv(obj, sep='\t')
+    df_pdl1['DTE_PATH_PROCEDURE'] = pd.to_datetime(df_pdl1['DTE_PATH_PROCEDURE'], errors='coerce')
+
+    
+    print('Loading %s' % fname_map)
+    obj = obj_minio.load_obj(path_object=fname_map)
+    df_map = pd.read_csv(obj, sep='\t')
+    
+    return df_pdl1, df_map
+
+
+def _clean_data_patient(df_pdl1):
+    df_pdl1 = df_pdl1.sort_values(by=['MRN', 'DTE_PATH_PROCEDURE'])
+    reps = {True:'Yes', False:'No'}
+    list_mrns_pdl1 = df_pdl1.loc[df_pdl1['PDL1_POSITIVE'] == 'Yes', 'MRN']
+    df_pdl1_summary = df_pdl1[['MRN']].drop_duplicates()
+    df_pdl1_summary['HISTORY_OF_PDL1'] = df_pdl1_summary['MRN'].isin(list_mrns_pdl1).replace(reps)
+    
+    return df_pdl1_summary
+
+
+def _clean_data_sample(
+    df_pdl1,
+    df_map
+):
+    df_pdl1_s1 = df_pdl1.merge(
+        right=df_map[['SAMPLE_ID', 'SOURCE_ACCESSION_NUMBER_0']], 
+        how='inner', 
+        left_on='ACCESSION_NUMBER', 
+        right_on='SOURCE_ACCESSION_NUMBER_0'
+    )
+    
+    df_pdl1_s = df_pdl1_s1[['SAMPLE_ID', 'PDL1_POSITIVE']].copy()
+    df_pdl1_s['DMP_ID'] = df_pdl1_s['SAMPLE_ID'].apply(lambda x: x[:9])
+    
+    return df_pdl1_s
+    
+    
+def create_pdl1_summaries(
+    fname_minio_env,
+    fname_pdl1,
+    fname_map,
+    fname_save_patient,
+    fname_save_sample
+):
+    # Create minio object
+    obj_minio = MinioAPI(fname_minio_env=FNAME_MINIO_ENV)
+    
+    # Load data
+    df_pdl1, df_map = _load_data(
+        obj_minio=obj_minio,
+        fname_pdl1=fname_pdl1,
+        fname_map=fname_map
+    )
+    
+    # Create summaries
+    ## Patient summary
+    df_pdl1_p = _clean_data_patient(df_pdl1=df_pdl1)
+    
+    ## Sample summary
+    df_pdl1_s = _clean_data_sample(
+        df_pdl1=df_pdl1, 
+        df_map=df_map
+    )
+    
+    # Save data
+    ## Patient summary
+    print('Saving %s' % fname_save_patient)
+    obj_minio.save_obj(
+        df=df_pdl1_p, 
+        path_object=fname_save_patient, 
+        sep='\t'
+    )
+    
+    ## Sample summary
+    print('Saving %s' % fname_save_sample)
+    obj_minio.save_obj(
+        df=df_pdl1_s, 
+        path_object=fname_save_sample, 
+        sep='\t'
+    )
+    
+    return True
+
+def main():
+    fname_minio_env = FNAME_MINIO_ENV
+    fname_pdl1 = FNAME_PDL1
+    fname_map = FNAME_MAP
+    fname_save_patient = FNAME_SAVE_PATIENT
+    fname_save_sample = FNAME_SAVE_SAMPLE
+    
+    print('Creating PD-L1 Summaries')
+    create_pdl1_summaries(
+        fname_minio_env=fname_minio_env,
+        fname_pdl1=fname_pdl1,
+        fname_map=fname_map,
+        fname_save_patient=fname_save_patient,
+        fname_save_sample=fname_save_sample
+    )
+    
+if __name__ == '__main__':
+    main()
+    
