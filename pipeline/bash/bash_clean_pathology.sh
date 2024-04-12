06895244b4d245a276441aa3abd6c96133b732e9
Fong <fongc2@pllimsksparky2.mskcc.org>
2024-04-12 14:01:42 -0400

    Fix on bash scri[pt

--- pipeline/bash/bash_clean_pathology.sh
+++ pipeline/bash/bash_clean_pathology.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#ROOT_PATH_PATHOLOGY_REPO=/gpfs/mindphidata/fongc2/github/pathology_report_segmentation/
 # Define which variables to use from msk_cdm.data_classes.<class> library
 PATH_PATHOLOGY_CLEAN=pipeline/pipeline_clean_pathology.py
 
@@ -10,6 +11,7 @@ source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
 conda activate conda-env-cdm
 
 SCRIPT_FULL_PATH="$ROOT_PATH_PATHOLOGY_REPO/$PATH_PATHOLOGY_CLEAN"
+echo $SCRIPT_FULL_PATH
 
 # Run script
 python $SCRIPT_FULL_PATH
