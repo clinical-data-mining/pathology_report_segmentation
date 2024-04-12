df9709977f04a82e0fc84e877fa94abaf822f370
Chris Fong <fongc2@mskcc.org>
2024-04-12 17:42:30 -0400

    Adding gleason extraction

--- src/pathology_report_segmentation/annotations/__init__.py
+++ src/pathology_report_segmentation/annotations/__init__.py
@@ -1,7 +1,9 @@
 from .pathology_id_mapping import create_id_mapping_pathology
 from .pathology_extract_pdl1 import PathologyExtractPDL1
+from .pathology_extract_gleason import extractGleason
 
 __all__ = [
     "create_id_mapping_pathology",
-    "PathologyExtractPDL1"
+    "PathologyExtractPDL1",
+    "extractGleason"
 ]
--- src/pathology_report_segmentation/annotations/pathology_extract_gleason.py
+++ src/pathology_report_segmentation/annotations/pathology_extract_gleason.py
@@ -1,21 +1,6 @@
-import pandas as pd
+# TODO: Migrate code from pipeline/pipeline_gleason_extraction.py into this script for cleaner code
 import numpy as np
 import re
-
-from msk_cdm.minio import MinioAPI
-from msk_cdm.data_processing import convert_to_int
-from msk_cdm.data_classes.legacy import CDMProcessingVariables as c_dar
-
-## Constants
-FNAME_SAVE = c_dar.fname_path_gleason
-FNAME_PATH = c_dar.fname_pathology
-FNAME_MINIO_ENV = c_dar.minio_env
-COLS_SAVE = ['MRN','Accession Number','Path Procedure Date','Gleason']
-
-print('Loading %s' % FNAME_PATH)
-obj_minio = MinioAPI(fname_minio_env=FNAME_MINIO_ENV)
-obj = obj_minio.load_obj(path_object=FNAME_PATH)
-df_path = pd.read_csv(obj, sep='\t', low_memory=False)
 
 
 def extractGleason(s):
@@ -50,29 +35,4 @@ def parsePostGleasonStr(s):
                 return int(tokens[1][0])
     return np.nan
 
-filter_gleason = df_path['path_prpt_p1'].fillna('').str.contains('Gleason',case=False)
-df_path_gleason = df_path[filter_gleason].copy()
-print('Abstracting Gleason scores')
-df_path_gleason['Gleason'] = df_path_gleason['path_prpt_p1'].apply(extractGleason)
-df_save = df_path_gleason[COLS_SAVE]
 
-# Do last cleaning -- Gleason scores should not be under 6. Convert 1's to 10 
-# TODO: Fix regex to grab 10s
-df_save.loc[df_save['Gleason'] == 1] = 10
-df_save.loc[df_save['Gleason'] < 6] = np.NaN
-df_save = df_save[df_save['Gleason'].notnull() & df_save['MRN'].notnull()]
-df_save = convert_to_int(df=df_save, list_cols=['MRN', 'Gleason'])
-
-
-print('Saving %s' % FNAME_SAVE)
-obj_minio.save_obj(
-    df=df_save, 
-    path_object=FNAME_SAVE, 
-    sep='\t'
-)
-# bytedata=.to_csv(index=False).encode('utf-8')
-# bufferdata=BytesIO(bytedata)
-
-# client.put_object('cdm-data', 'pathology/pathology_gleason_calls.tsv',
-#                   data=bufferdata,
-#                   length=len(bytedata),content_type='application/csv')
