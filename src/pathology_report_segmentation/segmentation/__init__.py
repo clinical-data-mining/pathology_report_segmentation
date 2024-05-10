995176ba4464564d6b53af7964b0fad49dddf025
Chris Fong <fongc2@mskcc.org>
2024-05-10 12:06:22 -0400

    Adding surgical specimens

--- src/pathology_report_segmentation/segmentation/__init__.py
+++ src/pathology_report_segmentation/segmentation/__init__.py
@@ -2,10 +2,12 @@ from .darwin_pathology import InitCleanPathology
 from .pathology_parse_surgical import ParseSurgicalPathology
 from .pathology_parse_molecular import ParseMolecularPathology
 from .pathology_parse_specimen_submitted import PathologyParseSpecSubmitted
+from .pathology_parsing_surgical_specimens import ParseSurgicalPathologySpecimens
 
 __all__ = [
     "InitCleanPathology",
     "ParseSurgicalPathology",
     "ParseMolecularPathology",
-    "PathologyParseSpecSubmitted"
+    "PathologyParseSpecSubmitted",
+    "ParseSurgicalPathologySpecimens"
 ]
