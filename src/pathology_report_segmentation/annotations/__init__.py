ef49093ecc3bf6f59a7d2b83001b6e377db61b2f
Chris Fong <fongc2@mskcc.org>
2024-05-10 13:03:18 -0400

    Adding date of procedure

--- src/pathology_report_segmentation/annotations/__init__.py
+++ src/pathology_report_segmentation/annotations/__init__.py
@@ -3,11 +3,13 @@ from .pathology_extract_pdl1 import PathologyExtractPDL1
 from .pathology_extract_gleason import extractGleason
 from .pathology_extract_mmr import extractMMR
 from .pathology_extract_accession import PathologyExtractAccession
+from .pathology_extract_dop import PathologyExtractDOP
 
 __all__ = [
     "create_id_mapping_pathology",
     "PathologyExtractPDL1",
     "extractGleason",
     "extractMMR",
-    "PathologyExtractAccession"
+    "PathologyExtractAccession",
+    "PathologyExtractDOP"
 ]
--- src/pathology_report_segmentation/annotations/pathology_extract_dop.py
+++ src/pathology_report_segmentation/annotations/pathology_extract_dop.py
@@ -1,8 +1,6 @@
 """"
 pathology_extract_dop.py
 
-By Chris Fong - MSKCC 2020
-
 This script will extract accession numbers that are
 buried in specimen submitted columns, or another specified column of text data
 
@@ -176,31 +174,6 @@ class PathologyExtractDOP(object):
 
         return df_sample_rpt_list1
 
-def main():
-    import sys
-    import os
-    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'cdm-utilities')))
-    from data_classes_cdm import CDMProcessingVariables as c_dar
 
-    ## Constants
-    col_label_access_num = 'ACCESSION_NUMBER'
-    col_label_spec_num = 'SPECIMEN_NUMBER'
-    col_spec_sub = 'SPECIMEN_SUBMITTED'
-
-    # Extract DOP
-    obj_p = PathologyExtractDOP(fname_minio_env=c_dar.minio_env,
-                                fname=c_dar.fname_darwin_path_col_spec_sub,
-                                col_label_access_num=col_label_access_num,
-                                col_label_spec_num=col_label_spec_num,
-                                col_spec_sub=col_spec_sub,
-                                list_accession=None,
-                                fname_save=c_dar.fname_spec_part_dop)
-
-    df = obj_p.return_df()
-
-    tmp = 0
-
-if __name__ == '__main__':
-    main()
 
 
