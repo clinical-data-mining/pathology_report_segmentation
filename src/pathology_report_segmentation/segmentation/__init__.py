2abf8bcfcb0646585cac0c0d70868e70700e20ab
Chris Fong <fongc2@mskcc.org>
2024-07-16 14:40:57 -0400

    Extracting clinical Dx

--- src/pathology_report_segmentation/segmentation/__init__.py
+++ src/pathology_report_segmentation/segmentation/__init__.py
@@ -3,11 +3,13 @@ from .pathology_parse_surgical import ParseSurgicalPathology
 from .pathology_parse_molecular import ParseMolecularPathology
 from .pathology_parse_specimen_submitted import PathologyParseSpecSubmitted
 from .pathology_parsing_surgical_specimens import ParseSurgicalPathologySpecimens
+from .pathology_parse_surgical_dx import parse_path_dx_section
 
 __all__ = [
     "InitCleanPathology",
     "ParseSurgicalPathology",
     "ParseMolecularPathology",
     "PathologyParseSpecSubmitted",
-    "ParseSurgicalPathologySpecimens"
+    "ParseSurgicalPathologySpecimens",
+    "parse_path_dx_section"
 ]
