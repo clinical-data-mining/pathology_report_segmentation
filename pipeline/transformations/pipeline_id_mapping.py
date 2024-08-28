64f2d328a184d2da1c6a90089ca345d6bb97f286
fongc2 <fongc2@mskcc.org>
2024-08-28 14:40:31 -0400

    Updating filename for saving id mapping

--- pipeline/transformations/pipeline_id_mapping.py
+++ pipeline/transformations/pipeline_id_mapping.py
@@ -6,7 +6,7 @@ def main():
     df_mapping = create_id_mapping_pathology(
         fname_minio_env=c_dar.minio_env,
         fname_path=c_dar.fname_path_clean,
-        fname_out_mapping=c_dar.fname_pid
+        fname_out_mapping=c_dar.fname_id_map
     )
 
 if __name__ == '__main__':
