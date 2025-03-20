e7228868e4c4ba51a89d907b988fd8881552e23d
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2025-03-20 12:42:55 -0400

    modify script to accept env vars from config file

--- pipeline/bash_run_python.sh
+++ pipeline/bash_run_python.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-#ROOT_PATH_PATHOLOGY_REPO=/gpfs/mindphidata/fongc2/github/pathology_report_segmentation/
-# Define which variables to use from msk_cdm.data_classes.<class> library
-#PATH_SCRIPT=pipeline/pipeline_id_mapping.py
-
 set -e
 
 # Activate virtual env
-source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
-conda activate conda-env-cdm
+source $CONDA_INSTALL_PATH/etc/profile.d/conda.sh
+conda activate $CONDA_ENV
 
 SCRIPT_FULL_PATH="$ROOT_PATH_PATHOLOGY_REPO/$PATH_SCRIPT"
 echo $SCRIPT_FULL_PATH
