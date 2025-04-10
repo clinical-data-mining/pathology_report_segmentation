3da85ddb4f4959af6956f1dc5b7a005c1981d5c9
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2025-04-10 16:35:00 -0400

    modify pipeline script to accept command line arguments

--- pipeline/bash_run_python.sh
+++ pipeline/bash_run_python.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-#ROOT_PATH_PATHOLOGY_REPO=/gpfs/mindphidata/fongc2/github/pathology_report_segmentation/
-# Define which variables to use from msk_cdm.data_classes.<class> library
-#PATH_SCRIPT=pipeline/pipeline_id_mapping.py
-
 set -e
+
+ROOT_PATH_PATHOLOGY_REPO=$1
+PATH_SCRIPT=$2
+
+test -n "$ROOT_PATH_PATHOLOGY_REPO"
+test -n "$PATH_SCRIPT"
 
 # Activate virtual env
 source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
