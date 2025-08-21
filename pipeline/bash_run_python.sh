856b627af84fff02eb63a6d1015fb8572c8610b8
fongc2 <fongc2@mskcc.org>
2025-08-21 13:18:41 -0400

    bug fix for running bash script in DAG

--- pipeline/bash_run_python.sh
+++ pipeline/bash_run_python.sh
@@ -6,6 +6,12 @@
 
 set -e
 
+ROOT_PATH_PATHOLOGY_REPO=$1
+PATH_SCRIPT=$2
+
+test -n "$ROOT_PATH_PATHOLOGY_REPO"
+test -n "$PATH_SCRIPT"
+
 # Activate virtual env
 source /gpfs/mindphidata/fongc2/miniconda3/etc/profile.d/conda.sh
 conda activate pathology-report-segmentation
