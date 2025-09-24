8620bf37a1f8c836b1b1e7ba7302e990b54ad333
fongc2 <fongc2@mskcc.org>
2025-09-24 15:36:15 -0400

    Adding test bash script to test locally for dbx use

--- pipeline/bash_run_python_minio_and_dbx_test.sh
+++ pipeline/bash_run_python_minio_and_dbx_test.sh
@@ -7,12 +7,14 @@ CONDA_INSTALL_PATH=/gpfs/mindphidata/fongc2/miniconda3
 CONDA_ENV_NAME=conda-env-cdm-fongc2
 MINIO_ENV=/gpfs/mindphidata/fongc2/minio_env_dev.txt
 PATH_SCRIPT=$1
+DBX_ENV=/gpfs/mindphidata/fongc2/databricks_env_prod.txt
 
 test -n "$ROOT_PATH_PATHOLOGY_REPO"
 test -n "$CONDA_INSTALL_PATH"
 test -n "$CONDA_ENV_NAME"
 test -n "$MINIO_ENV"
 test -n "$PATH_SCRIPT"
+test -n "$DBX_ENV"
 
 # Activate virtual env
 source $CONDA_INSTALL_PATH/etc/profile.d/conda.sh
@@ -22,4 +24,4 @@ SCRIPT_FULL_PATH="$ROOT_PATH_PATHOLOGY_REPO/$PATH_SCRIPT"
 echo $SCRIPT_FULL_PATH
 
 # Run script
-python $SCRIPT_FULL_PATH --minio_env=$MINIO_ENV
+python $SCRIPT_FULL_PATH --minio_env=$MINIO_ENV --databricks_env=$DBX_ENV
