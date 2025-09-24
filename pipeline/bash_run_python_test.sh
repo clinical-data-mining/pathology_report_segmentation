935262a73e3df470aeeaa74337e710d6c2d95c4b
fongc2 <fongc2@mskcc.org>
2025-09-24 15:33:45 -0400

    Adding test bash script to test locally

---
+++ pipeline/bash_run_python_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+ROOT_PATH_PATHOLOGY_REPO=/gpfs/mindphidata/cdm_repos_dev/dev/github/pathology_report_segmentation
+CONDA_INSTALL_PATH=/gpfs/mindphidata/fongc2/miniconda3
+CONDA_ENV_NAME=conda-env-cdm-fongc2
+MINIO_ENV=/gpfs/mindphidata/fongc2/minio_env_dev.txt
+PATH_SCRIPT=pipeline/transformations_epic/pipeline_extract_accession.py
+
+test -n "$ROOT_PATH_PATHOLOGY_REPO"
+test -n "$CONDA_INSTALL_PATH"
+test -n "$CONDA_ENV_NAME"
+test -n "$MINIO_ENV"
+test -n "$PATH_SCRIPT"
+
+# Activate virtual env
+source $CONDA_INSTALL_PATH/etc/profile.d/conda.sh
+conda activate $CONDA_ENV_NAME
+
+SCRIPT_FULL_PATH="$ROOT_PATH_PATHOLOGY_REPO/$PATH_SCRIPT"
+echo $SCRIPT_FULL_PATH
+
+# Run script
+python $SCRIPT_FULL_PATH --minio_env=$MINIO_ENV
