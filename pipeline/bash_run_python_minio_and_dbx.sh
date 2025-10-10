93dff53ef2befe71cba039774e7d5e85ed57fe28
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2025-10-10 17:27:54 -0400

    update order of bash args (#40)

--- pipeline/bash_run_python_minio_and_dbx.sh
+++ pipeline/bash_run_python_minio_and_dbx.sh
@@ -2,20 +2,19 @@
 
 set -e
 
-ROOT_PATH_PATHOLOGY_REPO=$1
-CONDA_INSTALL_PATH=$2
-CONDA_ENV_NAME=$3
-MINIO_ENV=$4
-DBX_ENV=$5
-PATH_SCRIPT=$6
+PATH_SCRIPT=$1
+ROOT_PATH_PATHOLOGY_REPO=$2
+CONDA_INSTALL_PATH=$3
+CONDA_ENV_NAME=$4
+MINIO_ENV=$5
+DATABRICKS_ENV=$6
 
+test -n "$PATH_SCRIPT"
 test -n "$ROOT_PATH_PATHOLOGY_REPO"
 test -n "$CONDA_INSTALL_PATH"
 test -n "$CONDA_ENV_NAME"
 test -n "$MINIO_ENV"
-test -n "$DBX_ENV"
-test -n "$PATH_SCRIPT"
-
+test -n "$DATABRICKS_ENV"
 
 # Activate virtual env
 source $CONDA_INSTALL_PATH/etc/profile.d/conda.sh
@@ -25,4 +24,4 @@ SCRIPT_FULL_PATH="$ROOT_PATH_PATHOLOGY_REPO/$PATH_SCRIPT"
 echo $SCRIPT_FULL_PATH
 
 # Run script
-python $SCRIPT_FULL_PATH --minio_env=$MINIO_ENV --databricks_env=$DBX_ENV
+python $SCRIPT_FULL_PATH --minio_env=$MINIO_ENV --databricks_env=$DATABRICKS_ENV
