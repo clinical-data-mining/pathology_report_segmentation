c83de83c694d7b0faef6a18749aa1b62df875067
fongc2 <fongc2@mskcc.org>
2026-01-05 15:02:01 -0500

    Removing additional columns

--- pipeline/transformations/pipeline_dop_extraction_epic.py
+++ pipeline/transformations/pipeline_dop_extraction_epic.py
@@ -60,7 +60,7 @@ def main():
     df_dop = obj_p.return_df()
     df_orig = obj_p.return_df_original()
 
-    df_f = df_orig.merge(right=df_dop, how='left', on=[COL_LABEL_1, COL_LABEL_2])
+    df_f = df_orig[[COL_LABEL_1, COL_LABEL_2]].merge(right=df_dop, how='left', on=[COL_LABEL_1, COL_LABEL_2])
     df_f = df_f.drop(columns=['DMP_ID'])
     df_f = df_f.rename(columns={'PDRX_ACCESSION_NO': 'ACCESSION_NUMBER'})
 
